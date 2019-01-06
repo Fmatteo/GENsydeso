a48f5a9fd26b40ae3096e360e0c6c592c3242b3e
Ming-desu <joshuaming.jm@gmail.com>
2019-01-06 10:21:02 -0300

    some updates

--- Sydeso/Main_Form.Designer.cs
+++ Sydeso/Main_Form.Designer.cs
@@ -28,20 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main_Form));
+            this.pnl_toolbar = new System.Windows.Forms.Panel();
+            this.btnExit = new System.Windows.Forms.PictureBox();
+            this.pnl_toolbar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnExit)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pnl_toolbar
+            // 
+            this.pnl_toolbar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.pnl_toolbar.Controls.Add(this.btnExit);
+            this.pnl_toolbar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnl_toolbar.Location = new System.Drawing.Point(0, 0);
+            this.pnl_toolbar.Name = "pnl_toolbar";
+            this.pnl_toolbar.Size = new System.Drawing.Size(1080, 40);
+            this.pnl_toolbar.TabIndex = 0;
+            this.pnl_toolbar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnl_toolbar_MouseDown);
+            this.pnl_toolbar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pnl_toolbar_MouseMove);
+            this.pnl_toolbar.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pnl_toolbar_MouseUp);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExit.Image = ((System.Drawing.Image)(resources.GetObject("btnExit.Image")));
+            this.btnExit.Location = new System.Drawing.Point(1057, 3);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(20, 20);
+            this.btnExit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnExit.TabIndex = 0;
+            this.btnExit.TabStop = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // Main_Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1080, 720);
+            this.Controls.Add(this.pnl_toolbar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Main_Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.pnl_toolbar.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.btnExit)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnl_toolbar;
+        private System.Windows.Forms.PictureBox btnExit;
     }
 }
 
--- Sydeso/Main_Form.cs
+++ Sydeso/Main_Form.cs
@@ -12,9 +12,36 @@ namespace Sydeso
 {
     public partial class Main_Form : Form
     {
+        private bool move;
+        private Point lastPoint;
+
         public Main_Form()
         {
             InitializeComponent();
+        }
+
+        private void pnl_toolbar_MouseDown(object sender, MouseEventArgs e)
+        {
+            move = true;
+            lastPoint = e.Location;
+        }
+
+        private void pnl_toolbar_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (move)
+            {
+                this.Location = new Point((this.Location.X - lastPoint.X) + e.X, (this.Location.Y - lastPoint.Y) + e.Y);
+            }
+        }
+
+        private void pnl_toolbar_MouseUp(object sender, MouseEventArgs e)
+        {
+            move = false;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
